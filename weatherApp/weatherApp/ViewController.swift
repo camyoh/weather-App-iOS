7a315d6e6ee59009e058561dd094acb65ad7c7fc
Carlos Mendieta <cmendieta@coadmins-MacBook-Pro-8.local>
2019-04-04 09:13:40 -0500

    adding project files

---
+++ weatherApp/weatherApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  weatherApp
+//
+//  Created by Carlos Mendieta on 4/4/19.
+//  Copyright © 2019 Carlos Mendieta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
